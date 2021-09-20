13a6b8b2bc2e4ea258269aae71112fe87194663c
nchavan007 <50837588+nchavan007@users.noreply.github.com>
2021-09-20 05:28:26 -0700

    Added support for xl

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3.6
 
-import mail
+import json
+from mail import sendmail
+from openpyxl import load_workbook
 
-sendmail(Title="Testmail")
+with open("Mail_Config", "r") as file:
+  MailDetails = json.load(file)
+DetailsXl = MailDetails["Path_XL"]
+subject =  MailDetails["DefaultSubject"]
+message = ""
+
+main_workbook = load_workbook(filename=DetailsXl)
+main_sheet = main_workbook.active
+for row in main_sheet.iter_rows(min_row=2, values_only=True):
+  message = f"""
+
+  test message for application {row[0]}
+  
+  """
+  
+  sendmail(Title=subject, receiver_email=row[1], msg=message)
