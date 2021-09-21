ba93888d655fd2b6f3cc6f93f97192ba4cbd7824
nchavan007 <50837588+nchavan007@users.noreply.github.com>
2021-09-20 22:46:43 -0700

    Final Changes

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ from mail import sendmail
 from openpyxl import load_workbook
 
 with open("Mail_Config", "r") as file:
-  MailDetails = json.load(file)
+    MailDetails = json.load(file)
 DetailsXl = MailDetails["Path_XL"]
-subject =  MailDetails["DefaultSubject"]
+sub = MailDetails["DefaultSubject"]
+folder_name = MailDetails["folder_name"]
 message = ""
 
 main_workbook = load_workbook(filename=DetailsXl)
 main_sheet = main_workbook.active
 for row in main_sheet.iter_rows(min_row=2, values_only=True):
-  message = f"""
+    print(row)
+    subject = sub + " |" + row[9]
+    file_name = f"sample_{row[9]}.eml"
+    message = f"""
+  test message for application {row[9]}
 
-  test message for application {row[0]}
-  
   """
-  
-  sendmail(Title=subject, receiver_email=row[1], msg=message)
+
+    sendmail(Title=subject, receiver_email=row[10], msg=message, folder_save=folder_name, file_name=file_name)
